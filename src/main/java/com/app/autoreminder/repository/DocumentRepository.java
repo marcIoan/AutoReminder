4f46a86bff3afc90acb8cbee517e17719076f931
marcIoan <marcdaian26@protonmail.com>
2020-06-02 15:45:33 +0300

    -Update

--- src/main/java/com/app/autoreminder/repository/DocumentRepository.java
+++ src/main/java/com/app/autoreminder/repository/DocumentRepository.java
@@ -58,7 +58,7 @@ public class DocumentRepository {
             }
             connection.close();
         }catch (SQLException ex) {
-            x.printStackTrace();
+            ex.printStackTrace();
         }
     }
 
