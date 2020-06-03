f787d0d8e3ae4571cd34c691627ac4d1b9dd977f
marcIoan <marcdaian26@protonmail.com>
2020-06-03 15:01:00 +0300

    -Update

--- src/main/java/com/app/autoreminder/ui/js/index.js
+++ src/main/java/com/app/autoreminder/ui/js/index.js
@@ -3,7 +3,7 @@ let Server_Url = "http://localhost:8080";
 
 
 function getCar() {
-    console.log("GET car request")
+    console.log("GET car request");
 
     //Make request to server to server in order to get the products
     return fetch(Server_Url + "/car", {
@@ -13,19 +13,20 @@ function getCar() {
 
             if (response.ok) {
                 return response.json();
+
             }
         }).then(function (car_list) {
         console.log(car_list);
 
-        let carListSelector = document.getElementById("#tableCar");
+        let carListSelector = document.querySelector("#tableL");
 
         for (let i = 0; i < car_list.length; i++) {
+
             let car = car_list[i];
 
             console.log("car" + car);
-
-            let table = document.getElementById("#tableCar");
-            let tableRow = table.insertRow();
+            let tbl = document.getElementById("tableL");
+            let tableRow = tbl.insertRow();
             let cell1 = tableRow.insertCell();
             let cell2 = tableRow.insertCell();
             let cell3 = tableRow.insertCell();
@@ -43,6 +44,13 @@ function getCar() {
             let buttonCarInfo = document.createElement("button");
             buttonCarInfo.className = "btn btn-info";
             buttonCarInfo.innerHTML = "Info";
+
+            buttonCarInfo.addEventListener('click', function (e) {
+                let idCar = e.target.getAtribute("car_id");
+                localStorage.setItem("prodId", idCar);
+                window.open('detail.html', '_self');
+            }, false);
+
 
             cell5.appendChild(buttonCarInfo);
             carListSelector.appendChild(tableRow);
